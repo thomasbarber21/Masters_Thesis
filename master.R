10c80e023fe0ba00c0195e174b3f98e317fcb32c
Tom Barber <barber.tom987@gmail.com>
2026-02-04 17:57:10 -0500

    Delete master.R

--- master.R
+++
@@ -1,8 +0,0 @@
-# Run the following code to create plots, clean data, and display regression results in txt format
-# May take a few minutes due to size of the data sets
-
-source("Preliminary.R")
-source("Data_Cleaning.R")
-source("Data_Visualization.R")
-source("synthetic_control.R")
-source("Regressions.R")
